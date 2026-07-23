98e85dd1a3ada7b4f269a96f2b56fdc7a9deeb42
Elior <heliosaian@gmail.com>
2026-07-23 20:53:02 +0300

    chore(plugin): Add plugin metadata, dependencies, and configuration for IntelliJ Platform

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,18 @@ import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 
 plugins {
   id("org.jetbrains.kotlin.jvm")
+  id("org.jetbrains.kotlin.plugin.compose")
+  id("org.jetbrains.kotlin.plugin.serialization")
+  id("org.jetbrains.compose")
   id("org.jetbrains.changelog")
   id("org.jetbrains.intellij.platform")
 }
 
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
 dependencies {
+  implementation(libs.kotlinx.serialization.json)
+
   testImplementation(libs.junit)
 
-  // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
   intellijPlatform {
     intellijIdea("2025.3.5")
     testFramework(TestFrameworkType.Platform)
@@ -19,5 +22,13 @@ dependencies {
     bundledPlugin("com.intellij.java")
     bundledPlugin("org.jetbrains.kotlin")
     bundledPlugin("JavaScript")
+
+    // Jewel (Compose for Desktop) ships bundled with the IntelliJ Platform since 251.2+.
+    // Rely on the platform-provided Compose runtime instead of bundling our own.
+    bundledModule("intellij.platform.jewel.foundation")
+    bundledModule("intellij.platform.jewel.ui")
+    bundledModule("intellij.platform.jewel.ideLafBridge")
+    bundledModule("intellij.libraries.compose.foundation.desktop")
+    bundledModule("intellij.libraries.skiko")
   }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,8 +3,15 @@ import org.jetbrains.intellij.platform.gradle.extensions.intellijPlatform
 rootProject.name = "clickUp"
 
 pluginManagement {
+  repositories {
+    gradlePluginPortal()
+    mavenCentral()
+  }
   plugins {
     id("org.jetbrains.kotlin.jvm") version "2.3.20"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.20"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.20"
+    id("org.jetbrains.compose") version "1.7.1"
     id("org.jetbrains.changelog") version "2.5.0"
   }
 }
@@ -16,6 +23,8 @@ plugins {
 dependencyResolutionManagement {
   // Configure all projects' repositories
   repositories {
+    google()
+    maven("https://packages.jetbrains.team/maven/p/kpm/public/")
     mavenCentral()
 
     // IntelliJ Platform Gradle Plugin Repositories Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-repositories-extension.html
